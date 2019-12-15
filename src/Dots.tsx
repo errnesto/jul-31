c6fbed4a3e53ddc0be9a1e82c132e7f049c1c61e
Manuel Reich <mr@wundf.net>
2019-12-15 18:20:37 +0100

    Use different scale

--- src/Dots.tsx
+++ src/Dots.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Chroma from 'chroma-js'
 import { interpolateRdPu } from 'd3-scale-chromatic'
 
-const dayNight = Chroma.scale(['#f7cf00', '#2c69a1'])
+const dayNight = Chroma.scale(['#f26f2a', '#ff9471', '#cccb4e', '#9fc9c2'])
 
 type Props = {
   hourlyData: Array<{
@@ -28,12 +28,12 @@ const Dots: React.FC<Props> = (props) => {
   ))
 
   const dotSize = 45
-  return <svg viewBox={`0 0 ${dotSize * 24 + dotSize / 2} ${dotSize * 28}`} width='100%' height='100%'>
+  return <svg viewBox={`0 0 ${dotSize * 28 * 0.9 + dotSize / 2} ${dotSize * 28}`} width='100%' height='100%' version="1.1" xmlns="http://www.w3.org/2000/svg">
     <defs>
       {windSpeeds.map(speed =>
         <filter id={`wind${speed}`} x='0%' y='0%' width='100' height='100'>
           <feTurbulence type='turbulence' baseFrequency='0.0 0.5' result='NOISE' numOctaves='5' />
-          <feDisplacementMap in='SourceGraphic' in2='NOISE' scale={speed * 2} xChannelSelector='R' yChannelSelector='R'></feDisplacementMap>
+          <feDisplacementMap in='SourceGraphic' in2='NOISE' scale={speed * 2.5} xChannelSelector='R' yChannelSelector='R'></feDisplacementMap>
         </filter>
       )}
       {rainHeights.map(height =>
@@ -43,28 +43,29 @@ const Dots: React.FC<Props> = (props) => {
       )}
     </defs>
     {hourlyData.map((v, i) => {
-      const isEvenLine = v.line % 2 === 0
+      const isEvenRow = i % 2 === 0
 
       const windSpeed = Math.floor(v.windSpeed)
       const rainHeight = Math.round(v.rain || 0)
       const sunshine = isNaN(v.sunshine) ? 0 : v.sunshine + 20
       const windFilter = windSpeed === 0 ? '' : `url(#wind${windSpeed})`
       const rainFilter = rainHeight === 0 ? '' : `url(#rain${rainHeight})`
-      const unshiftedX = v.hour * dotSize + dotSize / 2
-      const x = isEvenLine ? unshiftedX : unshiftedX + dotSize / 2
-      const y = dotSize * 0.9 * v.line + dotSize / 2
+      const unshifted = v.hour * dotSize + dotSize / 2
+      const y = (23 - v.hour) * dotSize + dotSize / 2
+      const x = isEvenRow
+        ? dotSize * v.line * 0.9 + dotSize / 2
+        : dotSize * v.line * 0.9 + dotSize / 2 + dotSize / 2
       const r = v.temperature * 0.6
 
       if (isNaN(r)) {
-        return <rect
+        return <g filter={windFilter}><rect
           x={x}
           y={y - 8}
           width='15'
           height='15'
           transform={`rotate(45, ${x}, ${y - 8})`}
           fill={interpolateRdPu(1.5 - (sunshine / 80 + 0.5) / 1.5)}
-          filter={windFilter}
-         />
+         /></g>
       }
 
       return <g filter={rainFilter}>
@@ -75,6 +76,7 @@ const Dots: React.FC<Props> = (props) => {
           r={r}
           overflow='visible'
           fill={interpolateRdPu(1.5 - (sunshine / 80 + 0.5) / 1.5)}
+          // fill={dayNight(1 - (sunshine / 80)).hex()}
           filter={windFilter} />
       </g>
     })}
